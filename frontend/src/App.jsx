8a0ea1d1ae5e4d013a20f29e6c86f8e9a0e34f40
linoahen-droi <linoahen@gmail.com>
2026-08-27 18:19:30 -0500

    mejora de enrutador app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/layout/Navbar.jsx'
-import Footer from './components/layout/Footer.jsx'
-import SearchRoutes from './pages/SearchRoutes.jsx'
-import SeatSelection from './pages/SeatSelection.jsx'
-import Checkout from './pages/Checkout.jsx'
-import Login from './pages/Login.jsx'
-import Dashboard from './pages/Dashboard.jsx'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BusMap from './components/bus/BusMap';
+import Checkout from './pages/Checkout';
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<SearchRoutes />} />
-            <Route path="/buscar" element={<SearchRoutes />} />
-            <Route path="/asientos/:routeId" element={<SeatSelection />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </main>
-        <Footer />
+    <Router>
+      <div className="min-h-screen bg-gray-100 py-12">
+        <Routes>
+          <Route path="/" element={<BusMap />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
       </div>
-    </BrowserRouter>
-  )
+    </Router>
+  );
 }
+
+export default App;
