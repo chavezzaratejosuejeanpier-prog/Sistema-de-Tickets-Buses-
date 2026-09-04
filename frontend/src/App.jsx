d992a1e1205948e90200f220ad703dc29db39272
linoahen-droi <linoahen@gmail.com>
2026-09-04 16:37:30 -0500

    feat: implementacion de Navbar y Footer para mejorar la estructura visual (UX)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 import BusMap from './components/bus/BusMap';
 import Checkout from './pages/Checkout';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100 py-12">
-        <Routes>
-          <Route path="/" element={<BusMap />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
+      {/* El Navbar va arriba de todo */}
+      <Navbar />
+      
+      {/* Contenedor principal para empujar el footer hacia abajo */}
+      <div className="min-h-screen bg-gray-100 flex flex-col">
+        <main className="flex-grow py-8">
+          <Routes>
+            <Route path="/" element={<BusMap />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </main>
+        
+        {/* El Footer va al final */}
+        <Footer />
       </div>
     </Router>
   );
